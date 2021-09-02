042c0ab7320314e44b2ee84cd97e47e19a2227e2
MaksSt <maksimsitnikov224@gmail.com>
2021-09-02 22:44:51 +0500

    Update Miner

--- js/click.js
+++ js/click.js
@@ -1,7 +1,8 @@
 
 var plus = 1
 var clicks = 0
-var upgrades1 = 0 
+var upgrades1 = 0
+var miners1 = 0
 
 cookieStorage = window.localStorage
 // Сколько добавляется
@@ -41,11 +42,23 @@ if (!cookieStorage.getItem('upgrades2'))
 upgrades2 = parseInt(cookieStorage.getItem('upgrades2'))
 document.getElementById("upgrades2").innerHTML = upgrades2
 
+//Майнер
+if (!cookieStorage.getItem('miners1'))
+    cookieStorage.setItem('miners1', '0')
+
+miners1 = parseInt(cookieStorage.getItem('miners1'))
+document.getElementById("miners1").innerHTML = miners1
+
 setInterval(() => {
-    cookieStorage.setItem('upgrades2', upgrades2)
+    cookieStorage.setItem('miners1', miners1)
 }, 10000);
 
 
+// Таймер майнера
+setInterval(() => {
+    clicks += miners1
+    document.getElementById("clicks").innerHTML = clicks
+}, 1000);
 
 // Функции кнопок
 
@@ -60,28 +73,28 @@ clicker.addEventListener('click', event => {
 
 const upgrademenu = document.getElementById('upg')
 upgrademenu.addEventListener('click', event => {
-    document.getElementById("upg").style.display='none'
-    document.getElementById("clicker").style.display='none'
-    document.getElementById("upgmenu").style.display='block'
-    document.getElementById("upgback").style.display='block'
+    document.getElementById("upg").style.display = 'none'
+    document.getElementById("clicker").style.display = 'none'
+    document.getElementById("upgmenu").style.display = 'block'
+    document.getElementById("upgback").style.display = 'block'
 })
 
 const back = document.getElementById('upgback')
 back.addEventListener('click', event => {
-    document.getElementById("upg").style.display='block'
-    document.getElementById("clicker").style.display='block'
-    document.getElementById("upgmenu").style.display='none'
-    document.getElementById("upgback").style.display='none'  
+    document.getElementById("upg").style.display = 'block'
+    document.getElementById("clicker").style.display = 'block'
+    document.getElementById("upgmenu").style.display = 'none'
+    document.getElementById("upgback").style.display = 'none'
 })
 
 // Кнопки улучшений
 
 const upgr1 = document.getElementById('upgrade1')
 upgr1.addEventListener('click', event => {
-    if (clicks >= 10){
-        clicks-=10 
+    if (clicks >= 10) {
+        clicks -= 10
         plus += 2
-        upgrades1 +=1
+        upgrades1 += 1
         document.getElementById("upgrades1").innerHTML = upgrades1
         document.getElementById("plus").innerHTML = plus
         document.getElementById("clicks").innerHTML = clicks
@@ -90,12 +103,32 @@ upgr1.addEventListener('click', event => {
 
 const upgr2 = document.getElementById('upgrade2')
 upgr2.addEventListener('click', event => {
-    if (clicks >= 20000){
-        clicks-=20000
+    if (clicks >= 20000) {
+        clicks -= 20000
         plus += 10000
-        upgrades2 +=1
+        upgrades2 += 1
         document.getElementById("upgrades2").innerHTML = upgrades2
         document.getElementById("plus").innerHTML = plus
         document.getElementById("clicks").innerHTML = clicks
-    } 
+    }
+})
+
+const mine1 = document.getElementById('miner1')
+mine1.addEventListener('click', event => {
+    if (clicks >= 5000) {
+        clicks -= 5000
+        miners1 += 250
+        document.getElementById("clicks").innerHTML = clicks
+        document.getElementById("miners1").innerHTML = miners1
+    }
+})
+
+const mine2 = document.getElementById('miner2')
+mine2.addEventListener('click', event => {
+    if (clicks >= 30000) {
+        clicks -= 30000
+        miners1 += 25000
+        document.getElementById("clicks").innerHTML = clicks
+        document.getElementById("miners1").innerHTML = miners1
+    }
 })
